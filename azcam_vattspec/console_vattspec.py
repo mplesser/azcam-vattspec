34cfc8a27797bd0c559fdd328e28d067de19d510
Michael Lesser <mlesser@arizona.edu>
2022-11-24 09:33:26 -0700

    monorepo

--- azcam_vattspec/console_vattspec.py
+++ azcam_vattspec/console_vattspec.py
@@ -7,8 +7,8 @@ import threading
 import azcam
 import azcam.console
 import azcam.shortcuts
-from azcam_ds9.ds9display import Ds9Display
-from azcam_focus.focus import Focus
+from azcam.tools.ds9.ds9display import Ds9Display
+from azcam.tools.focus.focus import Focus
 
 try:
     i = sys.argv.index("-datafolder")
--- azcam_vattspec/server_vattspec.py
+++ azcam_vattspec/server_vattspec.py
@@ -7,15 +7,16 @@ import azcam.shortcuts
 from azcam.tools.cmdserver import CommandServer
 from azcam.tools.system import System
 from azcam.tools.instrument import Instrument
+from azcam.tools.arc.controller_arc import ControllerArc
+from azcam.tools.arc.exposure_arc import ExposureArc
+from azcam.tools.arc.tempcon_arc import TempConArc
+from azcam.tools.ds9.ds9display import Ds9Display
+from azcam.tools.imageserver.sendimage import SendImage
+from azcam.tools.fastapi.fastapi_server import WebServer
+from azcam.tools.webtools.status.status import Status
+
 from azcam_monitor.monitorinterface import AzCamMonitorInterface
-from azcam_arc.controller_arc import ControllerArc
-from azcam_arc.exposure_arc import ExposureArc
-from azcam_arc.tempcon_arc import TempConArc
-from azcam_ds9.ds9display import Ds9Display
-from azcam_imageserver.sendimage import SendImage
 from azcam_vattspec.telescope_vatt import VattTCS
-from azcam_fastapi.fastapi_server import WebServer
-from azcam_webtools.status.status import Status
 
 # ****************************************************************
 # parse command line arguments
